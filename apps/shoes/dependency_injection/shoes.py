c4330026b2fef1bb78466d7d6c43019dd3286573
Roberto Garcia Navarro <info@soulcodex.es>
2023-08-03 23:22:23 +0200

    Add command bus to shoes dependency injection module

--- apps/shoes/dependency_injection/shoes.py
+++ apps/shoes/dependency_injection/shoes.py
@@ -4,16 +4,18 @@ from fastapi import Depends
 from aiologger import Logger
 from typing_extensions import Annotated
 from typing import AsyncIterator, Type, cast
-from shoes.domain.types import ShoesQueryBus
 from shoes.domain.shoe import ShoesRepository
 from shared.domain.bus.query_bus import QueryBus
+from shared.domain.bus.command_bus import CommandBus
+from shoes.domain.types import ShoesQueryBus, ShoesCommandBus
 from shoes.application.find_shoe_by_id import FindShoeByIdQuery, FindShoeByIdQueryHandler
 from shoes.infrastructure.persistence.mysql_shoes_repository import MysqlShoesRepository
 
 from apps.shoes.dependency_injection.common import \
     configure_database_connection_pool, \
     configure_logger, \
-    configure_query_bus
+    configure_query_bus, \
+    configure_command_bus
 
 
 async def shoes_repository(
@@ -29,3 +31,10 @@ async def shoes_query_bus(
 ) -> ShoesQueryBus:
     await bus.register_query(FindShoeByIdQuery, FindShoeByIdQueryHandler(repository=repo))
     return cast(ShoesQueryBus, bus)
+
+
+async def shoes_command_bus(
+        _: Annotated[ShoesRepository, Depends(shoes_repository)],
+        bus: Annotated[CommandBus, Depends(configure_command_bus)]
+) -> ShoesCommandBus:
+    return cast(configure_command_bus, bus)
--- shoes/domain/types.py
+++ shoes/domain/types.py
@@ -1,4 +1,6 @@
 from typing import NewType
 from shared.domain.bus.query_bus import QueryBus
+from shared.domain.bus.command_bus import CommandBus
 
 ShoesQueryBus = NewType('ShoesQueryBus', QueryBus)
+ShoesCommandBus = NewType('ShoesCommandBus', CommandBus)
